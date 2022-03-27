b7aad6177857150a21842bed997b8052b3ff36c7
gahusb <bgg8988@gmail.com>
2022-03-27 23:56:10 +0900

    algo problem_02_06

--- algorithm_learn/problem_02_06.java
+++ algorithm_learn/problem_02_06.java
@@ -1,16 +1,42 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class problem_02_06 {
-    public int solution(int N) {
-        int answer = 0;
-        
+    public boolean isPrime(int num) {
+        if(num == 1) return false;
+        for(int i = 2; i < num; i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
 
-        return answer;
+    public ArrayList<Integer> solution(int N, int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < N; i++) {
+            int tmp = arr[i];
+            int res = 0;
+            while(tmp > 0) {
+                int t = tmp % 10;
+                res = res * 10 + t;
+                tmp = tmp / 10;
+            }
+
+            if(isPrime(res)) list.add(res);
+        }
+
+        return list;
     }
     public static void main(String[] args) {
         problem_02_06 mc = new problem_02_06();
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        System.out.println(mc.solution(N));
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int x : mc.solution(N, arr)) {
+            System.out.print(x + " ");
+        }
     }
 }
