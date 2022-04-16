4027db36367a14e11d5664edd97bd4fa49b23c4e
gahusb <bgg8988@gmail.com>
2022-04-16 23:31:08 +0900

    daily algorithms solve

---
+++ baekjoon_online/AlleyBossHoseok_20183.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class AlleyBossHoseok_20183 {
+    static int N, M, A, B, C;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        
+    }
+}
