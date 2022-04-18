98d44c5042acf742bd484e48a6765ed06470f6ac
gahusb <bgg8988@gmail.com>
2022-04-18 21:58:11 +0900

    java algorithms corse problem solve

---
+++ algorithm_learn/problem_03_06.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class problem_03_06 {
+    public int solution(int n, int k, int[] arr) {
+        int result = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++) {
+            int tmp = k;
+            int tmpLen = 0;
+            for(int j = i; j < n; j++) {
+                if(arr[j] == 0) {
+                    if(tmp == 0) break;
+                    tmp--;
+                    tmpLen++;
+                } else tmpLen++;
+            }
+            if(result < tmpLen) result = tmpLen;
+        }
+
+        return result;
+    }
+    public static void main(String[] args) throws IOException {
+        problem_03_06 mc = new problem_03_06();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(mc.solution(N, K, arr));
+    }
+}
